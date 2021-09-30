c579575ff0edd7f16ff5078122ee8d394c1a70ea
Prasanna Anjankar <prasannaanjankar@gmail.com>
2021-09-30 16:00:54 +0530

    fin

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,30 +1,48 @@
-import React from 'react';
-function Navbar() {
-    return (
-        <header style={{ position: "sticky", top: 0 }}>
-            <div className="brandName">
-                Prsn.
-            </div>
-            <nav className="mobileMenuList">
-                <ul>
-                    <a href="#about">
-                        <li         >
-                            About
-                        </li>
-                    </a>
-                    <a href="#skills">
-                        <li>
-                            Skills
-                        </li>
-                    </a>
-                    <a href="#projects">
-                        <li>
-                            Projects
-                        </li>
-                    </a>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+import React, { useState, useEffect } from "react";
+
+const Navbar = () => {
+  const [activeSection, setActiveSection] = useState("");
+  const handleScrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+  useEffect(() => {
+    setActiveSection(document.location.hash);
+  }, []);
+  return (
+    <header style={{ position: "sticky", top: 0 }}>
+      <div className="brandName" onClick={handleScrollToTop}>
+        swnnd.
+      </div>
+      <nav className="mobileMenuList">
+        <ul>
+          <a href="#about">
+            <li
+              className={activeSection === "#about" ? "activeSection" : null}
+              onClick={() => setActiveSection("#about")}
+            >
+              About
+            </li>
+          </a>
+          <a href="#skills">
+            <li
+              className={activeSection === "#skills" ? "activeSection" : null}
+              onClick={() => setActiveSection("#skills")}
+            >
+              Skills
+            </li>
+          </a>
+          <a href="#projects">
+            <li
+              className={activeSection === "#projects" ? "activeSection" : null}
+              onClick={() => setActiveSection("#projects")}
+            >
+              Projects
+            </li>
+          </a>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 export default Navbar;
