26ac53ca725df4c12c41d8c7a663efc57167b020
Prasanna Anjankar <prasannaanjankar@gmail.com>
2021-09-30 17:01:14 +0530

    changesmaode

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from"./Components/navbar"
 import Home from"./Components/home"
 import About from './Components/about'
 import Skills from './Components/skills'
-import Footer from "./components/footer";
+import Footer from "./Components/footer"
 import Projects from './Components/project'
 
 function App() {
